fd34a979020a808a67e730942b63cdbb94386b97
inaiworldsweb <inaiworldsweb@gmail.com>
2026-01-26 18:53:48 +0530

    2.0

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 
 import ReactDOM from 'react-dom/client'
-import { HelmetProvider } from 'react-helmet'
 import App from './App.jsx'
 import './index.css'
 import './styles/globals.css'
@@ -8,8 +7,6 @@ import React from 'react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>,
 )
